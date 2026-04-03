2a1de2824f166c2af8ed4d29f4cac5b07b55b9ad
clairenicholsondigital <139775904+clairenicholsondigital@users.noreply.github.com>
2026-04-03 15:27:25 +0100

    Update db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,12 +1,7 @@
-import os
 import psycopg
 from contextlib import contextmanager
 
-# Use env var if available, fallback to working Docker hostname
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://cupboardchef:Tennis2!@postgresql:5432/cupboardchef_db"
-)
+DATABASE_URL = "postgresql://cupboardchef:Tennis2!@postgresql:5432/cupboardchef_db"
 
 @contextmanager
 def get_conn():
